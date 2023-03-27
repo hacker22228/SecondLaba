27e7192affdff7d99ffcee5c268e36d2e27825af
hacker228 <113432931+hacker22228@users.noreply.github.com>
2023-03-27 08:55:07 +0300

    SecondLaba

---
+++ WpfBD/Person.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfBD.WPFDBDataSetTableAdapters;
+
+namespace WpfBD
+{
+    /// <summary>
+    /// Логика взаимодействия для Person.xaml
+    /// </summary>
+    public partial class Person : Page
+    {
+        public PersonTableAdapter adapter = new PersonTableAdapter();
+        public Company comp;
+        public Person()
+        {
+            InitializeComponent();
+        }
+
+        private void GoTab2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(comp);
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            adapter.InsertQuery(FName.Text, LName.Text, Convert.ToInt32(Age.Text));
+            PersonDataGrid.ItemsSource = adapter.GetData();
+        }
+    }
+}
